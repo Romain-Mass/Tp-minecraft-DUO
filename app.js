c5f40f14af3bf990c6aff0a9736506f66bf667b0
Romain <roro.95220@gmail.com>
2026-05-12 16:56:07 +0200

    Part 02

--- app.js
+++ app.js
@@ -2,6 +2,17 @@
 const $btnSearch = document.querySelector(".btn-search");
 const $AllMobs = document.querySelector(".mob-found");
 const $Not = document.querySelector(".notFound");
+const $menu = document.querySelector("#menu");
+const $exit = document.querySelector("#exit");
+const $dialog = document.querySelector("#dialog");
+
+$menu.addEventListener("click", () => {
+	$dialog.showModal();
+});
+
+$exit.addEventListener("click", () => {
+	$dialog.close();
+});
 
 $btnSearch.addEventListener("click", (e) => {
 	e.preventDefault();
@@ -14,7 +25,10 @@ $btnSearch.addEventListener("click", (e) => {
 });
 
 async function loadAllMobs() {
-	const response = await fetch("http://10.69.4.208:3000/v1/entities");
+	const response = await fetch("http://10.69.4.208:3000/v1/entities", {
+		method: "GET",
+		body: JSON.stringify(),
+	});
 	const data = await response.json();
 	console.log(data);
 }
@@ -32,10 +46,19 @@ function createCard() {
 	// $div2.classList.add("");
 
 	$Name.textContent = loadAllMobs.name;
+	$line.src = loadAllMobs.image;
+	$SeeMore.textContent = "See More";
+	$class.textContent = loadAllMobs.classification;
+	$type.textContent = loadAllMobs.type;
 
 	$AllMobs.appendChild($div);
 	$div.appendChild($div2);
 	$div2.appendChild($Name);
+	$div2.appendChild($line);
+	$div.appendChild($div3);
+	$div3.appendChild($class);
+	$div3.appendChild($type);
+	$div.appendChild($SeeMore);
 
 	return $AllMobs;
 }
